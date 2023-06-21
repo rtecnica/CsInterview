67fa35bfa99f599081a184677a65c730f3a9880c
rtecnica <imaldonado@ing.uchile.cl>
2023-06-21 13:21:28 -0400

    addmodule.sh typo fix

--- dataStructures/addmodule.sh
+++ dataStructures/addmodule.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir "$SCRIPT_DIR/$1"
 mkdir "$SCRIPT_DIR/$1/inc"
 mkdir "$SCRIPT_DIR/$1/src"
+mkdir "$SCRIPT_DIR/$1/bin"
 touch "$SCRIPT_DIR/$1/$1.py"
 touch "$SCRIPT_DIR/$1/inc/lib.h"
 echo "#include \"../inc/lib.h\"" > "$SCRIPT_DIR/$1/src/lib.c"
