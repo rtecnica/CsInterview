074b14be26ea992c26394f103ffdddee045fe060
imaldonado <imaldonado@wiseconn.com>
2023-11-24 00:58:49 -0300

    Update

--- coding_challenge_qubika/coding_challenge.c
+++ coding_challenge_qubika/coding_challenge.c
@@ -11,6 +11,12 @@ char s1[] = {0x02, 0x11, 0x31, 0x32, 0x2E, 0x31, 0x03, 0x02, 0x13, 0x32, 0x03, 0
 char s2[] = {0x02, 0x11, 0x31, 0x32, 0x2E, 0x31, 0x03, 0x02, 0x13, 0x32, 0x03, 0x02, 0x02, 0x12, 0x33, 0x03,
              0x03, 0x02, 0x0A, 0x03, 0x02, 0x18, 0x03, 0x02, 0x11, 0x32, 0x30, 0x30, 0x03, 0x02, 0x0A, 0x03};
 
+enum state {
+  WAIT_START,
+  PARSING_OPCODE,
+  PARSING_OPERAND,
+};
+
 enum OPCODE {
   ADD = 0x11,
   SUBS = 0x12,
@@ -20,56 +26,28 @@ enum OPCODE {
   RST = 0x18,
 };
 
-enum statescodes {
-  pSTX,
-  pOPC,
-  pOPE_ETX,
-};
-
-enum retcodes {
-  OK,
-  FAIL,
-  REPEAT,
-  SRT_OVER,
-};
-
-struct parsed_op {
+struct operation {
   uint8_t opcode;
   float operand;
-} parsed_op;
-
-struct parsed_operand {
-  uint8_t length;
-  char buf[100];
-} parsed_operand;
+} operation;
 
 float accumulator;
-
-typedef struct transition {
-  enum statescodes src;
-  enum retcodes ret;
-  enum statescodes dest;
-} transitions;
-
-transitions state_transitions[] = {
-    {pSTX, OK, pOPC},       {pOPC, OK, pOPE_ETX},       {pOPE_ETX, REPEAT, pOPE_ETX}, {pOPE_ETX, OK, pSTX},
-    {pSTX, FAIL, pSTX},     {pOPC, FAIL, pSTX},         {pOPE_ETX, FAIL, pSTX},       {pSTX, SRT_OVER, pOPC},
-    {pOPC, SRT_OVER, pOPC}, {pOPE_ETX, SRT_OVER, pOPC},
-};
+char buf[100];
+uint8_t buf_len;
 
 void accumulate(void) {
-  switch (parsed_op.opcode) {
+  switch (operation.opcode) {
   case ADD:
-    accumulator += parsed_op.operand;
+    accumulator += operation.operand;
     break;
   case SUBS:
-    accumulator -= parsed_op.operand;
+    accumulator -= operation.operand;
     break;
   case MUL:
-    accumulator = accumulator * parsed_op.operand;
+    accumulator = accumulator * operation.operand;
     break;
   case DIV:
-    accumulator = accumulator / parsed_op.operand;
+    accumulator = accumulator / operation.operand;
     break;
   case PRNT:
     printf("%f\n", accumulator);
@@ -78,96 +56,66 @@ void accumulate(void) {
     accumulator = 0;
     break;
   default:
-
     return;
   }
   return;
 }
 
-// STATE FUNCTIONS
-int pSTX_state(char curr_char) {
-  if (curr_char != STX)
-    return FAIL;
-  return OK;
-}
+uint8_t state_machine(uint8_t state, char input) {
+  switch (state) {
+  case WAIT_START:
+    if (input == STX)
+      return PARSING_OPCODE;
+    break;
+  case PARSING_OPCODE:
+    switch (input) {
+    case ADD:
+      break;
+    case SUBS:
+      break;
+    case MUL:
+      break;
+    case DIV:
+      break;
+    case PRNT:
+      break;
+    case RST:
+      break;
+    default:
+      return WAIT_START;
+      break;
+    }
+    operation.opcode = (uint8_t)input;
+    return PARSING_OPERAND;
+    break;
 
-int pOPC_state(char curr_char) {
-  switch (curr_char) {
-  case ADD:
-    parsed_op.opcode = ADD;
-    break;
-  case SUBS:
-    parsed_op.opcode = SUBS;
-    break;
-  case MUL:
-    parsed_op.opcode = MUL;
-    break;
-  case DIV:
-    parsed_op.opcode = DIV;
-    break;
-  case PRNT:
-    parsed_op.opcode = PRNT;
-    break;
-  case RST:
-    parsed_op.opcode = RST;
-    break;
-  case STX:
-    return SRT_OVER;
-    break;
-  default:
-    return FAIL;
+  case PARSING_OPERAND:
+    if (input == ETX) {
+      buf[buf_len] = 0;
+      buf_len = 0;
+      sscanf(buf, "%f", &(operation.operand));
+      accumulate();
+      return WAIT_START;
+    }
+    if (((input < '0') || (input > '9')) && (input != '.'))
+      return WAIT_START;
+    buf[buf_len] = input;
+    buf_len++;
+    return PARSING_OPERAND;
     break;
   }
-  return OK;
+  return WAIT_START;
 }
 
-int pOPE_state(char curr_char) {
-  uint8_t isValidNum = ((curr_char >= '0') && (curr_char <= '9') || curr_char == '.');
-  if (isValidNum) {
-    parsed_operand.buf[parsed_operand.length] = curr_char;
-    parsed_operand.length++;
-    return REPEAT;
-  } else if (curr_char == ETX) {
-    parsed_operand.buf[parsed_operand.length] = 0x00;
-    parsed_operand.length = 0;
-
-    sscanf(parsed_operand.buf, "%f", &(parsed_op.operand));
-    accumulate();
-    return OK;
-  } else {
-    parsed_operand.length = 0;
-    return FAIL;
-  }
-}
-
-int (*state[])(char) = {pSTX_state, pOPC_state, pOPE_state};
-
-// AUX FUNCS
-enum statescodes transition_lookup(enum statescodes cur_state, enum retcodes ret_code) {
-  int length = sizeof(state_transitions) / sizeof(transitions);
-  for (int i = 0; i < length; i++) {
-    if ((state_transitions[i].src == cur_state) && (state_transitions[i].ret == ret_code))
-      return state_transitions[i].dest;
-  }
-  return pSTX;
-}
-
-void process_packets(char* data, int len) {
-  int (*state_fun)(char);
-  enum statescodes current_state = pSTX;
-  enum retcodes rc;
-
+void process_packets(char* stream, size_t str_len) {
   accumulator = 0;
-  parsed_operand.length = 0;
-  parsed_op.opcode = -1;
-  parsed_op.operand = 0;
-
-  for (int i = 0; i < len; i++) {
-    state_fun = state[current_state];
-    rc = state_fun(data[i]);
-    current_state = transition_lookup(current_state, rc);
+  operation.opcode = 0;
+  operation.operand = 0;
+  buf_len = 0;
+  uint8_t curr_state = WAIT_START;
+  for (size_t i = 0; i < str_len; i++) {
+    curr_state = state_machine(curr_state, stream[i]);
   }
-  return;
 }
 
 int main(void) {
